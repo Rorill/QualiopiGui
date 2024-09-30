c580f3a38d104a647790a0e339dca18d93b9de01
Guillaume Herold <guillaumeherold@gmail.com>
2024-09-30 10:38:47 +0200

    added some function in controller

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,7 +12,16 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // get the login error if there is one
+        if ($this->getUser()) {
+            if ($this->isGranted('ROLE_ADMIN')) {
+                return $this->redirectToRoute('admin_dashboard');
+            } else {
+                return $this->redirectToRoute('user_dashboard'); // Remplace par une route appropriée pour les autres utilisateurs
+            }
+        }
+
+
+    // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
         // last username entered by the user
