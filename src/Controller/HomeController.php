895d40d5c7a8da9aee57c7b3a2d9f1c8744837a5
Guillaume Herold <guillaumeherold@gmail.com>
2024-09-30 14:35:59 +0200

    created admin main dashboard

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class HomeController extends AbstractController
 {
@@ -14,6 +15,18 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+    }
+
+    public function loggedin()
+    {
+
+        if ($this->getUser()) {
+            if ($this->isGranted('ROLE_ADMIN')) {
+                return $this->redirectToRoute('app_admin');
+            } else {
+                return $this->redirectToRoute('app_user');
+            }
+        }
     }
 }
 
