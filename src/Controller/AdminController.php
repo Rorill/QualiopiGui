f2476f3048721c35f5e717d79b67eb9cb5c4f7be
Guillaume Herold <guillaumeherold@gmail.com>
2024-10-02 11:56:14 +0200

    made admin user registration form

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,6 +5,7 @@ use App\Entity\Documents;
 use App\Entity\Formations;
 use App\Entity\User;
 use App\Form\CsvImportType;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,6 +13,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 
 class AdminController extends AbstractController
@@ -94,6 +97,37 @@ class AdminController extends AbstractController
         ]);
     }
 
+
+    //route to add User
+
+    #[Route('/admin/user/add', name: 'admin_user_add')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function addUser(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // password hash
+            $user->setPassword(
+                $passwordHasher->hashPassword($user, $user->getPassword())
+            );
+            $user->setRoles(['ROLE_USER']);  // Default role
+
+
+            // Register user in db
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Utilisateur créé avec succès.');
+            return $this->redirectToRoute('app_admin');
+        }
+
+        return $this->render('admin/app_adduser.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
 
 
 
